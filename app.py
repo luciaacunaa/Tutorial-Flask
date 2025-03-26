7a40a30787d5fe263d576941fa9f1b6103dc99ea
Lucia Acuña <luciaacunaaaa@gmail.com>
2025-03-26 12:11:21 -0300

    Hicimos una ruta que pueda sumar dos numeros y de el resultado.

--- app.py
+++ app.py
@@ -3,5 +3,23 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>lupucipi!</p>"
+def principal():
+    return """
+     <a href='/jiji'>jiji</a> 
+     <a href='/chau'>!!</a>
+    """
+
+@app.route("/jiji")
+def jijiji():
+    return "<h2>MUEJEJEJE</h2>"
+
+@app.route("/chau")
+def jijijint():
+    return "<h2>muejejeje</h2>"
+
+@app.route("/sumar/<int:n1>/<int:n2>")
+def suma(n1, n2):
+    suma = n1+n2
+    return F"<h2>{n1} más {n2} es igual a {suma}</h2>"
+
+    
